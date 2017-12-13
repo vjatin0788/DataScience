f991dd6f7d18f7ae9fc015473717682daccfea0d
vjatin0788 <34059084+vjatin0788@users.noreply.github.com>
2017-12-13 19:12:59 +0530

    the prediction with simple linear regression

---
+++ simple_linear_reg_demo.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 13 17:41:03 2017
+
+@author: jpuri
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+# Importing the dataset
+#os.getcwd()
+#os.chdir('D:\\Users\\jpuri\\Desktop\\My Work\\\Machine Learning A-Z\\Part 2 - Regression\\Section 4 - Simple Linear Regression')
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values #matrix of 2D is required (Experience)
+y = dataset.iloc[:, 1].values #vector of 1D is required because it is dependent variable.(Salary)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+#fitting the linear regression model in the training set.
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
+
+#predicting the salary on test case
+y_pred = regressor.predict(X_test)
